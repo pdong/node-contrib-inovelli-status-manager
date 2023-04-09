1d2f15c9dd3d2f2809b0b8d0f224cfb69d146280
Phi Dong <phidong@gmail.com>
2023-04-08 22:12:12 -0700

    Add support for LZW30-SN

--- README.md
+++ README.md
@@ -1,8 +1,33 @@
-#### Inovelli Status Manager Node
+## Inovelli Status Manager Node
+
 This node allows you to easily set the appropriate value to send to your Inovelli red series switch.
 
-It should be used in conjunction with the node-red–contrib-home-assistant add on and the output should
-be connected to a api-call-service node that is set to do a `svc: zwave:set_config_parameter`.  The values
-from this node will get merged into the `svc: set_config_parameter`.
+### Z-wave (red) switches w/ zwave_js
 
-If you notice any problems open an issue or a pull request.  Thanks.
+This node should be used in conjunction with the `node-red–contrib-home-assistant` add on and the output
+should be connected to a api-call-service node that is set to the domain `zwave_js` and the service `bulk_set_partial_config_parameters`.
+You can set the entity ID to your z-wave switch. DO NOT set the Node ID on the status manager node as it isn't needed
+when you set `bulk_set_partial_config_parameters` to point to an entity id.
+
+### Quick start guide
+
+There are example flows for red switches to zwave_js service call nodes and blue switches to either native mqtt out or home assistant `mqtt.publish` nodes
+
+### Zigbee (blue) switches
+
+There is currently support for constructing a MQTT payload for full LED control for Zigbee2MQTT users. (Individual control might be coming soon but will be trickier).
+When using this node with home assistant it can be connected directly to an `api-call-service` node calling `mqtt` with the service `publish`.
+You will want to set the topic to `zigbee2mqtt/FRIENDLY_NAME/set`.
+
+#### Node-RED mqtt out node
+
+You can wire the configration node directly to the mqtt out node. You just need to set the topic ``zigbee2mqtt/FRIENDLY_NAME/set`.
+
+#### Home Assistant MQTT Publish node
+
+The `mqtt.publish` service only supports string payloads so you have to stringify the output and pass it to the payload. There is an example
+that does this using a function node but there are multiple ways to do this (e.g. )
+
+### Contributing
+
+If you notice any problems open an issue or a pull request. Thanks.
--- test/inovelli-status.spec.js
+++ test/inovelli-status.spec.js
@@ -1,67 +1,196 @@
 var helper = require("node-red-node-test-helper");
-var assert = require("assert");
+var should = require("should");
 var statusNode = require("../inovelli-status");
- 
-helper.init(require.resolve('node-red'));
- 
-describe('inovelli-status node', function () {
- 
+
+helper.init(require.resolve("node-red"));
+
+describe("inovelli-status node", function () {
   beforeEach(function (done) {
-      helper.startServer(done);
+    helper.startServer(done);
   });
- 
+
   afterEach(function (done) {
-    helper.unload().then(function() {
-      helper.stopServer(done);
-    });
+    helper.unload();
+    helper.stopServer(done);
   });
- 
-  it('should be loaded', function (done) {
-    var flow = [{ id: "n1", type: "inovelli-status-manager", name: "inovelli-status-manager" }];
-    helper.load(statusNode, flow, function () {
-      var n1 = helper.getNode("n1");
-      assert.equal(n1.name, "inovelli-status-manager");
-      done();
-    });
-  });
- 
-  it('should always send parameter of 8', function (done) {
+
+  it("should be loaded", function (done) {
     var flow = [
-      { id: "n1", type: "inovelli-status-manager", name: "inovelli-status-manager", wires:[["n2"]] },
-      { id: "n2", type: "helper" }
+      {
+        id: "n1",
+        type: "inovelli-status-manager",
+        name: "inovelli-status-manager",
+      },
     ];
     helper.load(statusNode, flow, function () {
-      var n2 = helper.getNode("n2");
-      var n1 = helper.getNode("n1");
-      n2.on("input", function (msg) {
-        assert.equal(msg.payload.data && msg.payload.data.parameter, 8);
+      const n1 = helper.getNode("n1");
+
+      try {
+        n1.should.have.property("name", "inovelli-status-manager");
         done();
-      });    
+      } catch (err) {
+        done(err);
+      }
+    });
+  });
+
+  it("should omit node_id if not configured and send the correct parameter with a value", function (done) {
+    var flow = [
+      {
+        id: "n1",
+        type: "inovelli-status-manager",
+        name: "inovelli-status-manager",
+        color: 0,
+        level: 10,
+        duration: 30,
+        display: 1,
+        switchtype: 8,
+        wires: [["n2"]],
+      },
+      { id: "n2", type: "helper" },
+    ];
+    helper.load(statusNode, flow, function () {
+      const n2 = helper.getNode("n2");
+      const n1 = helper.getNode("n1");
+      n2.on("input", function (msg) {
+        try {
+          msg.payload.data.should.not.have.property("node_id");
+          msg.payload.data.should.have.property("parameter", 8);
+          msg.payload.data.should.have.property("value").which.is.a.Number();
+          done();
+        } catch (err) {
+          done(err);
+        }
+      });
       n1.receive({ payload: {} });
     });
   });
 
-  it('should allow setting of all the status properties from the receiving payload', function (done) {
+  it("should cap the duration at 255 for zwave switches", function (done) {
     var flow = [
-      { id: "n1", type: "inovelli-status-manager", name: "inovelli-status-manager", wires:[["n2"]] },
-      { id: "n2", type: "helper" }
+      {
+        id: "n1",
+        type: "inovelli-status-manager",
+        name: "inovelli-status-manager",
+        color: 0,
+        level: 0,
+        duration: 266,
+        display: 0,
+        switchtype: "LZW30-SN",
+        wires: [["n2"]],
+      },
+      { id: "n2", type: "helper" },
+    ];
+    helper.load(statusNode, flow, function () {
+      const n2 = helper.getNode("n2");
+      const n1 = helper.getNode("n1");
+      n2.on("input", function (msg) {
+        try {
+          msg.payload.data.should.have.property("value", 255 * 65536);
+          done();
+        } catch (err) {
+          done(err);
+        }
+      });
+      n1.receive({ payload: {} });
+    });
+  });
+
+  it("should send a MQTT style payload for zigbee switches", function (done) {
+    var flow = [
+      {
+        id: "n1",
+        type: "inovelli-status-manager",
+        name: "inovelli-status-manager",
+        color: 82,
+        level: 10,
+        duration: 266,
+        display: "fast_rising",
+        switchtype: "VZM31-SN",
+        wires: [["n2"]],
+      },
+      { id: "n2", type: "helper" },
+    ];
+    helper.load(statusNode, flow, function () {
+      const n2 = helper.getNode("n2");
+      const n1 = helper.getNode("n1");
+      n2.on("input", function (msg) {
+        try {
+          msg.payload.should.have.property("led_effect");
+          msg.payload.led_effect.should.have.property("effect", "fast_rising");
+          msg.payload.led_effect.should.have.property("color", 82);
+          msg.payload.led_effect.should.have.property("level", 100);
+          msg.payload.led_effect.should.have.property("duration", 64);
+          done();
+        } catch (err) {
+          done(err);
+        }
+      });
+      n1.receive({ payload: {} });
+    });
+  });
+
+  it("should set duration to 255 for zigbee switches that pass over 482401 seconds", function (done) {
+    var flow = [
+      {
+        id: "n1",
+        type: "inovelli-status-manager",
+        name: "inovelli-status-manager",
+        color: 82,
+        level: 10,
+        duration: 482402,
+        display: "small_to_big",
+        switchtype: "VZM31-SN",
+        wires: [["n2"]],
+      },
+      { id: "n2", type: "helper" },
+    ];
+    helper.load(statusNode, flow, function () {
+      const n2 = helper.getNode("n2");
+      const n1 = helper.getNode("n1");
+      n2.on("input", function (msg) {
+        try {
+          msg.payload.led_effect.should.have.property("duration", 255);
+          done();
+        } catch (err) {
+          done(err);
+        }
+      });
+      n1.receive({ payload: {} });
+    });
+  });
+
+  it("should allow setting of all the status properties from the receiving payload", function (done) {
+    var flow = [
+      {
+        id: "n1",
+        type: "inovelli-status-manager",
+        name: "inovelli-status-manager",
+        wires: [["n2"]],
+      },
+      { id: "n2", type: "helper" },
     ];
     helper.load(statusNode, flow, function () {
       var n2 = helper.getNode("n2");
       var n1 = helper.getNode("n1");
       n2.on("input", function (msg) {
-        assert.equal(msg.payload.data.node_id, 20);
-        done();
-      });    
-      n1.receive({ 
+        try {
+          msg.payload.data.should.have.property("node_id", 20);
+          msg.payload.data.should.have.property("parameter", 16);
+          done();
+        } catch (err) {
+          done(err);
+        }
+      });
+      n1.receive({
         payload: {
           nodeId: 20,
           color: 1,
           duration: 10,
           display: 1,
           level: 10,
-          switchtype: 8
-        }
+          switchtype: 16,
+        },
       });
     });
   });
